1311df3b3b111766d215dbb84d920cb30b9c82ae
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2023-10-23 19:09:30 +0100

    Update: understanding how the index.js react app works

--- src/index.js
+++ src/index.js
@@ -11,6 +11,24 @@ root.render(
   </React.StrictMode>
 );
 
+// If I was using React version17, the code block above would look like this:
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+//   , document.getElementById('root'));
+
+// But on the latest version of react, it has changed. This is because the .render function is no longer supported on the latest version. It has been replaced with the .createRoot function.
+
+//Also, the lastest way of doing this can also be re-written as so:
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
