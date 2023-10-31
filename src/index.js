05908a7e11fa87e80cfaa0ddc5b9a1ccbfc4a3fa
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2023-10-31 21:10:29 +0100

    feat: creating first react component using class & defining props

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <h1> Hello, World! </h1>
     <App />
+    <Hello greeting={'Hell, ' + 'React Ninja'} />
   </React.StrictMode>
 );
 
