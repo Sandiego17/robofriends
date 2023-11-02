d0e1fdeda30022f85a78b7179c47682ffc816201
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2023-11-02 17:04:51 +0100

    update: declaring the id, name & email props as a variable

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 const Card = (props) => {
+  const { id, name, email} = props;
   return (
     // <h1>RoboFriends</h1>
     <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc">
-      <img alt="robots" src={`https://robohash.org/${props.id}`} />
+      <img alt="robots" src={`https://robohash.org/${id}`} />
       <div>
-        <h2>{ props.name }</h2>
-        <p>{ props.email }</p>
+        <h2>{ name }</h2>
+        <p>{ email }</p>
       </div>
     </div>
   );
