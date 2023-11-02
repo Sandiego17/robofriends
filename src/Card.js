4dd3c9afc81a414069a996a866cd406e6073c574
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2023-11-02 17:06:28 +0100

    update: destructuring the props

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,6 @@
 import React from "react";
 
-const Card = (props) => {
-  const { id, name, email} = props;
+const Card = ({ id, name, email}) => {
   return (
     // <h1>RoboFriends</h1>
     <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc">
