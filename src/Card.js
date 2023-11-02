c8b6d05e023283cc2784184cac9a6a286338db40
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2023-11-02 17:03:15 +0100

    Feat: dynamically changing the API with the data IDs

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ const Card = (props) => {
   return (
     // <h1>RoboFriends</h1>
     <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc">
-      <img alt="robots" src="https://robohash.org/test" />
+      <img alt="robots" src={`https://robohash.org/${props.id}`} />
       <div>
         <h2>{ props.name }</h2>
         <p>{ props.email }</p>
